d0ab3bffb64473cdbc7a89e6d336ed2101f99ca7
Hamlet Tamazian <hamlettamaz@gmail.com>
2016-09-21 11:40:32 -0700

    trying to render sidenav directly to the body

---
+++ imports/ui/components/CentralDiv.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default React.createClass({
+	render: function() {
+		return (
+			<div className='centralDiv' >
+				<h1>Welcome to Open Canvassing</h1>
+			</div>
+		)
+	}
+})
